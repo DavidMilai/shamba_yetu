590798ca2c0acc5a4247ddcfa720eb684cbbdb2a
DavidMilai <davidmilai67@gmail.com>
2020-06-09 11:58:55 +0300

    Used a traditional appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,103 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'results.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../components/listTileWithAction.dart';
+import 'profile.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 150.0,
-              floating: true,
-              //pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  background: Image.asset(
-                    'images/vegies.jpg',
-                    fit: BoxFit.cover,
-                  )),
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(60),
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Text("Shamba Yetu",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 30.0,
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Container(
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Icon(
-                                    Icons.search,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 5),
-                              Container(
-                                child: Text(
-                                  'what are you looking for?',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ),
-                              SizedBox(width: 40),
-                              Container(
-                                child: IconButton(
-                                    icon: Icon(
-                                      Icons.settings,
-                                      color: Colors.green[400],
-                                    ),
-                                    onPressed: () {}),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ];
-        },
-        body: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Card(
-                child: ListTile(
-                  onTap: () {},
-                  leading: FlutterLogo(),
-                  title: Text('One-line with leading widget'),
-                ),
-              ),
-            ),
-            Theme(
-                data:
-                    Theme.of(context).copyWith(canvasColor: Colors.transparent),
-                child: TileWithAction()),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Home'),
       ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(3.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {},
+                leading: FlutterLogo(),
+                title: Text('One-line with leading widget'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Profile(),
+                    ),
+                  );
+                },
+                leading: FlutterLogo(),
+                title: Text('Profile'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
   }
 }
---
+++ lib/pages/profile.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+import 'package:vegymarket/pages/sell_page.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> with TickerProviderStateMixin {
+  List<Widget> container = [
+    Container(color: Colors.blueAccent),
+    Container(color: Colors.green),
+    Container(color: Colors.yellow),
+  ];
+  TabController tabcontroller;
+
+  @override
+  void initState() {
+    super.initState();
+    tabcontroller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                child: Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment(0, -0.95),
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundImage: AssetImage('images/profile.jpg'),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(0, 1),
+                      child: TabBar(
+                          controller: tabcontroller,
+                          unselectedLabelColor: Colors.black,
+                          labelColor: Colors.green,
+                          tabs: <Widget>[
+                            Text(
+                              'Ads',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              'Favorites',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              'About You',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          ]),
+                    ),
+                    Positioned(
+                      left: 20,
+                      top: 50,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'Recent Views',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text('50',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ))
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      right: 20,
+                      top: 50,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'Total Ads',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text('4',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ))
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(0, 0),
+                      child: Text(
+                        'Harry Potter',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(0, 0.25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '0701181025',
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //edit sell messages
+                    Align(
+                      alignment: Alignment(0, 0.6),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          OutlineButton(
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.green[400],
+                            ),
+                            padding: EdgeInsets.all(8),
+                            shape: CircleBorder(),
+                          ),
+                          ButtonTheme(
+                            minWidth: 150,
+                            child: RaisedButton(
+                              elevation: 10,
+                              color: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => SellPage(),
+                                  ),
+                                );
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(top: 10, bottom: 10),
+                                child: Text(
+                                  'Sell',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                          OutlineButton(
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.message,
+                              color: Colors.green[400],
+                            ),
+                            padding: EdgeInsets.all(8),
+                            shape: CircleBorder(),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: tabcontroller,
+                  children: <Widget>[
+                    ListView(
+                      physics: BouncingScrollPhysics(),
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 2, right: 8, left: 8, bottom: 2),
+                          child: Card(
+                            color: Colors.greenAccent,
+                            elevation: 5,
+                            child: ListTile(
+                              leading: FlutterLogo(size: 50),
+                              title: Text('My add'),
+                              subtitle: Text('This is my first ad.'),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.green[300],
+                              ),
+                              //isThreeLine: true,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 2, right: 8, left: 8, bottom: 2),
+                          child: Card(
+                            elevation: 5,
+                            child: ListTile(
+                              leading: FlutterLogo(size: 50),
+                              title: Text('My add'),
+                              subtitle: Text('This is my first ad.'),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.green[300],
+                              ),
+                              //isThreeLine: true,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 2, right: 8, left: 8, bottom: 2),
+                          child: Card(
+                            elevation: 5,
+                            child: ListTile(
+                              leading: FlutterLogo(size: 50),
+                              title: Text('My add'),
+                              subtitle: Text('This is my first ad.'),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.green[300],
+                              ),
+                              //isThreeLine: true,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 2, right: 8, left: 8, bottom: 2),
+                          child: Card(
+                            elevation: 5,
+                            child: ListTile(
+                              leading: FlutterLogo(size: 50),
+                              title: Text('My add'),
+                              subtitle: Text('This is my first ad.'),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.green[300],
+                              ),
+                              //isThreeLine: true,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 2, right: 8, left: 8, bottom: 2),
+                          child: Card(
+                            elevation: 5,
+                            child: ListTile(
+                              leading: FlutterLogo(size: 50),
+                              title: Text('My add'),
+                              subtitle: Text('This is my first ad.'),
+                              trailing: Icon(
+                                Icons.delete,
+                                color: Colors.green[300],
+                              ),
+                              //isThreeLine: true,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 5,
+                      color: Colors.green[100],
+                    ),
+                    Container(height: 5, color: Colors.yellow),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
