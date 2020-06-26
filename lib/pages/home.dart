a4031ca7703c72ce2132a1f2ad6b018cea007008
DavidMilai <davidmilai67@gmail.com>
2020-06-26 20:22:00 +0300

    Home screen UI design

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,40 +5,111 @@ import 'profile.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      appBar: AppBar(elevation: 0),
+      drawer: Drawer(
+        child: ListView(padding: EdgeInsets.zero, children: [
+          DrawerHeader(child: Text("Header")),
+          ListTile(title: Text("Home"))
+        ]),
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(3.0),
-            child: Card(
-              child: ListTile(
-                onTap: () {},
-                leading: FlutterLogo(),
-                title: Text('One-line with leading widget'),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Card(
-              child: ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Profile(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: size.height * 0.15,
+              child: Stack(
+                children: [
+                  Container(
+                    height: size.height * 0.15 - 20,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                      ),
                     ),
-                  );
-                },
-                leading: FlutterLogo(),
-                title: Text('Profile'),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20, bottom: 10),
+                      child: Text(
+                        'Hi what are you looking for?\n Username',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.symmetric(horizontal: 30),
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(40),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 10),
+                              blurRadius: 50,
+                              color: Colors.green.withOpacity(0.23),
+                            ),
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          suffixIcon: Icon(Icons.search),
+                          hintStyle: TextStyle(
+                            color: Colors.green.withOpacity(0.5),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          print(value);
+                        },
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.all(3.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {},
+                  leading: FlutterLogo(),
+                  title: Text('One-line with leading widget'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Profile(),
+                      ),
+                    );
+                  },
+                  leading: FlutterLogo(),
+                  title: Text('Profile'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
